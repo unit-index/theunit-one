f48579a6df812466524b9d1e2e1376bf1c7c5f6b
ShuaJJ <Sody008@gmail.com>
2023-05-29 14:49:14 +0800

    removed react reveal

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -9,7 +9,6 @@ import { useTranslations } from 'next-intl'
 import indexFund from '@/public/index-fund.png'
 import Blogs from '@/components/Blogs'
 import Accounted from '@/components/Accounted'
-import FadeWrapper from '@/components/FadeWrapper'
 import { whiteTrans } from '@/utils/TranslationHelper'
 import clientPromise from '@/utils/mongodb'
 import { getUnitHourlyData } from "@/utils/db";
@@ -74,18 +73,15 @@ function Home({
 
 
     <div className='bg-home-bgd1 bg-no-repeat bg-left pt-16 pb-16'>
-    {/* -------------------- Value Accounted ------------------ */}
-    <FadeWrapper isMobile={isMobile}>
+      {/* -------------------- Value Accounted ------------------ */}
       <div className='lg:flex items-center gap-24 justify-center my-32'>
         <Accounted title={t('accounted-in', {unit: 'UNIT'})} />
         <div className='w-0 h-12 lg:block lg:w-[1px] lg:h-7 bg-gray-light'></div>
         <Accounted title={t('accounted-in', {unit: 'USD'})} usdData={usdData} />
       </div>
-    </FadeWrapper>
 
 
-    {/* -------------------- Partners Section ------------------ */}
-    <FadeWrapper isMobile={isMobile}>
+      {/* -------------------- Partners Section ------------------ */}
       <div className='block xl:flex items-center xl:gap-48 p-8 xl:p-32 bg-partners bg-cover mt-20'>
         <div className='block text-center mb-12 xl:mb-0 xl:text-left xl:flex-1'>
           <div className='text-4xl font-semibold mb-4 text-white'>{t('with-support')}</div>
@@ -97,74 +93,64 @@ function Home({
           <Partners />
         </div>
       </div>
-    </FadeWrapper>
     </div>
 
 
     {/* -------------------- UNIT Ø Introduction ------------------ */}
-    <FadeWrapper isMobile={isMobile}>
-      <div className='flex items-center gap-40 px-8 lg:px-40 py-24 mb-32'>
-        {!isMobile && <div className='flex-1 pointer-events-none h-96'>
-          <SplineClient url='https://prod.spline.design/noP1fbdLdVrd-p58/scene.splinecode' />
-        </div>}
-        <div className='text-center lg:text-left flex-1'>
-          <div className='text-4xl font-semibold mb-4 text-white'>UNIT Ø</div>
-          <div className='text-xl'>
-            {t.rich('unit-algorithm', whiteTrans)}
-          </div>
-          <div className='block sm:flex items-center justify-center lg:justify-start gap-2 pt-2'>
-            <LinkButton 
-              title={t.rich('launch-app', whiteTrans)} 
-              link={appUrl} 
-              small 
-            />
-            <div className='text-xl mt-2 sm:mt-0'>
-              {t.rich('to-play', whiteTrans)}
-            </div>
+    <div className='flex items-center gap-40 px-8 lg:px-40 py-24 mb-32'>
+      {!isMobile && <div className='flex-1 pointer-events-none h-96'>
+        <SplineClient url='https://prod.spline.design/noP1fbdLdVrd-p58/scene.splinecode' />
+      </div>}
+      <div className='text-center lg:text-left flex-1'>
+        <div className='text-4xl font-semibold mb-4 text-white'>UNIT Ø</div>
+        <div className='text-xl'>
+          {t.rich('unit-algorithm', whiteTrans)}
+        </div>
+        <div className='block sm:flex items-center justify-center lg:justify-start gap-2 pt-2'>
+          <LinkButton 
+            title={t.rich('launch-app', whiteTrans)} 
+            link={appUrl} 
+            small 
+          />
+          <div className='text-xl mt-2 sm:mt-0'>
+            {t.rich('to-play', whiteTrans)}
           </div>
         </div>
       </div>
-    </FadeWrapper>
+    </div>
     
 
     {/* -------------------- Youtube Channel ------------------ */}
-    <FadeWrapper isMobile={isMobile}>
-      <div className='text-4xl text-center font-semibold mb-4 text-white'>{t('our-channel')}</div>
-      <div className='text-xl text-center max-w-[44rem] mx-auto mb-7 w-full'>{t.rich('channel-intro', whiteTrans)}</div>
-      <iframe
-        src="https://www.youtube.com/embed/VP1kvgJheR8?controls=0"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        className='max-w-4xl mx-auto w-full aspect-video shadow-2xl shadow-white/20'
-      ></iframe>
-    </FadeWrapper>
+    <div className='text-4xl text-center font-semibold mb-4 text-white'>{t('our-channel')}</div>
+    <div className='text-xl text-center max-w-[44rem] mx-auto mb-7 w-full'>{t.rich('channel-intro', whiteTrans)}</div>
+    <iframe
+      src="https://www.youtube.com/embed/VP1kvgJheR8?controls=0"
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      className='max-w-4xl mx-auto w-full aspect-video shadow-2xl shadow-white/20'
+    ></iframe>
 
     {/* -------------------- Index Table ------------------ */}
-    <FadeWrapper isMobile={isMobile}>
-      <div className='px-8 lg:px-32 py-16 my-40 relative'>
-        <div className='absolute left-0 lg:left-32 bottom-16 top-16 right-32 bg-none md:bg-index bg-no-repeat bg-left-bottom rounded-lg' />
-        <div className='flex items-center gap-20 bg-black-bgd/40 backdrop-blur-sm rounded-lg border border-gray-border px-1 md:px-9 py-8 md:py-24'>
-          <div className='flex-1 px-3 lg:pl-11 lg:pr-0 text-center lg:text-left'>
-            <div className='text-4xl font-semibold mb-4 text-white'>{t('traders-investors')}</div>
-            <div className='text-xl'>{t.rich('manage-fund-desc', whiteTrans)}</div><br />
-            <div className='text-xl'>{t.rich('borrow-unit-desc', whiteTrans)}</div>
-          </div>
-          <div className='hidden lg:block flex-1'>
-            <Image src={indexFund} alt='Index Fund' className='w-full' />
-          </div>
+    <div className='px-8 lg:px-32 py-16 my-40 relative'>
+      <div className='absolute left-0 lg:left-32 bottom-16 top-16 right-32 bg-none md:bg-index bg-no-repeat bg-left-bottom rounded-lg' />
+      <div className='flex items-center gap-20 bg-black-bgd/40 backdrop-blur-sm rounded-lg border border-gray-border px-1 md:px-9 py-8 md:py-24'>
+        <div className='flex-1 px-3 lg:pl-11 lg:pr-0 text-center lg:text-left'>
+          <div className='text-4xl font-semibold mb-4 text-white'>{t('traders-investors')}</div>
+          <div className='text-xl'>{t.rich('manage-fund-desc', whiteTrans)}</div><br />
+          <div className='text-xl'>{t.rich('borrow-unit-desc', whiteTrans)}</div>
+        </div>
+        <div className='hidden lg:block flex-1'>
+          <Image src={indexFund} alt='Index Fund' className='w-full' />
         </div>
       </div>
-    </FadeWrapper>
+    </div>
 
     {/* -------------------- From the blog ------------------ */}
-
-    <FadeWrapper isMobile={isMobile}>
-      <Blogs 
-        readMore={t('read-more')} 
-        title={t('from-blog')} 
-        subtitle={t.rich('blog-desc', whiteTrans)} 
-      />
-    </FadeWrapper>
+    <Blogs 
+      readMore={t('read-more')} 
+      title={t('from-blog')} 
+      subtitle={t.rich('blog-desc', whiteTrans)} 
+    />
   </>
 }
