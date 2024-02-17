817be0942ddbe2830e20cb34e51c781f463cbcbf
ShuaJJ <Sody008@gmail.com>
2024-02-17 15:51:36 +0800

    get latest data for unit usd value

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,4 @@
 export async function getUnitHourlyData(db) {
-    const lastData = await db.collection('hourlydatausds').find().sort({ "time": -1 }).limit(1).toArray();
+    const lastData = await db.collection('fiveminutedatausds').find().sort({ "time": -1 }).limit(1).toArray();
     return lastData[0].value;
 }
