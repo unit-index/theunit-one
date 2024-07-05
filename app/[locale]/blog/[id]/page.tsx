1f7e30bf5395dbd10a3483d6cec911d7456919b5
cyber-joshua <joshua@cybertinolab.com>
2024-07-05 09:54:38 +0800

    removed log

--- app/[locale]/blog/[id]/page.tsx
+++ app/[locale]/blog/[id]/page.tsx
@@ -31,7 +31,6 @@ export async function generateMetadata(
   { params }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
-  console.log('AAAA', params);
 
   if (!params.id) {
     return {};
